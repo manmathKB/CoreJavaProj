99af7e7e51f2929cecfbff9a6bb77e44b9ae7b35
mkbDev <chagul.mkb@gmail.com>
2019-04-17 22:15:59 +0530

    DaemonCheck.java updated

--- BasicJavaProjects/src/com/javaThread/daemon/DaemonCheck.java
+++ BasicJavaProjects/src/com/javaThread/daemon/DaemonCheck.java
@@ -1,13 +1,16 @@
 package com.javaThread.daemon;
 
+import java.util.Scanner;
+
 public class DaemonCheck {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		sc.nextLine();
 		UserNonDaemonThread undth=null;
 		
 		undth=new UserNonDaemonThread();
 		undth.start();
-	
 	}
 
 }
